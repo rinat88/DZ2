5186af8ac635573c4b9f4a4352488ee788950cf7
rinat88 <riki06m@gmail.com>
2021-08-24 20:50:18 +0300

    Доделал 2 ДЗ

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,32 +5,45 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(gg());
         int age = 33;
         int temp = 15;
-
         if (age < 45 && age > 10 && temp < 30 && temp > -10) {
             System.out.println("go for a walk");
-        }
-else if (age < 10 && temp > 0 && temp < 8) {
+        } else if (age < 10 && temp > 0 && temp < 8) {
             System.out.println("go for a walk , but not for a long time");
-        }
-
-else if (age < 65 && temp < -10 && temp > 25)
-
-    {
-        System.out.println("stay home");
-    }
-else {
+        } else if (age < 65 && temp < -10 && temp > 25) {
+            System.out.println("stay home");
+        } else {
             System.out.println("stay home!");
         }
-
-
+        System.out.println(gg());
+        sw();
+    }
+    public static void sw(){
+        String name1 = "Kkk";
+        String name2 = "Hhh";
+        String name3 = "Lll";
+        switch (name1){
+            case "Kkk":
+                System.out.println(name1);
+                break;
+            case "Hhh":
+                System.out.println(name2);
+                break;
+            case "Lll":
+                System.out.println(name3);
+                break;
+            default:
+                System.out.println("No this name" );
+        }
 
     }
-    public static String gg (){
-        String nameOfHuman = "Sam";
+
+    public static String gg() {
+        String nameOfHuman = "Sam ";
         int ageOfHuman = 50;
-        return  nameOfHuman + " " + ageOfHuman;
+        String info = nameOfHuman + ageOfHuman;
+        return info;
+
     }
 }
